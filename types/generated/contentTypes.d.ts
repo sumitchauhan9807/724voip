f2e036a6e7aaf593b82e77d11e98605437e3dbf1
MuhamedK112 <muhamed@madebykaizen.com>
2026-07-16 17:29:18 +0200

    Teams pages fix

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -949,6 +949,8 @@ export interface ApiTeamDetailTeamDetail extends Struct.CollectionTypeSchema {
     > &
       Schema.Attribute.Private;
     publishedAt: Schema.Attribute.DateTime;
+    slug: Schema.Attribute.UID<'title'> & Schema.Attribute.Required;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
